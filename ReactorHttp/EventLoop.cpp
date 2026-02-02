075c9b178d094fc502482dddaad64714a0bf2597
wz <1437175102@qq.com>
2026-02-02 20:04:09 +0800

    添加本地套接字通信

--- ReactorHttp/EventLoop.cpp
+++ ReactorHttp/EventLoop.cpp
@@ -3,6 +3,8 @@
 #include "EpollDispatcher.h"
 #include <sys/socket.h>
 #include <cassert>
+#include <unistd.h>
+#include <string.h>
 
 EventLoop::EventLoop()
 	:EventLoop(std::string()){}
@@ -19,7 +21,18 @@ EventLoop::EventLoop(const std::string threadName) {
 		perror("socketpair");
 		exit(0);
 	}
+	
+	auto obj = std::bind(&EventLoop::readMessage, this);
+	Channel* channel = new Channel(
+		m_socketPair[1],
+		FDEvent::ReadEvent,
+		obj,
+		nullptr,
+		nullptr,
+		this
+	);	
 
+	addTask(channel, ElemType::ADD);
 }
 
 EventLoop::~EventLoop() {
@@ -64,8 +77,84 @@ int EventLoop::addTask(Channel* channel, ElemType type) {
 	m_mutex.unlock();
 	//处理节点
 	if(m_threadID == std::this_thread::get_id()) {
+		//如果是子线程处理任务队列
+		//这里也可能是主线程发起的添加监听lfd
 		processTaskQ();
 	} else {
+		//这里一定是主线程 >> 第一次new connection的时候会走到这里, 所有不会出现通信自己的本地套接字情况
 		taskWakeup();
 	}
 }
+
+int EventLoop::processTaskQ() {
+	while (!m_taskQ.empty()) {
+		m_mutex.lock();
+	}
+	ChannelElement* node = m_taskQ.front();
+	m_taskQ.pop();
+	m_mutex.unlock();
+	Channel* channel = node->channel;
+	if (node->type == ElemType::ADD) {
+		add(channel);
+	}
+	else if (node->type == ElemType::DELETE) {
+		delete(channel);
+	}
+	else if(node->type == ElemType::MODIFY){
+		modify(channel);
+	}
+}
+
+int EventLoop::add(Channel* channel) {
+	int fd = channel->getSocket();
+	if(m_channelMap.find(fd) == m_channelMap.end()) {
+		m_channelMap.insert(std::make_pair(fd, channel));
+		//todo...
+		int ret = m_dispatcher->add();
+		return ret;
+	}
+	return -1;
+}
+
+int EventLoop::modify(Channel* channel) {
+	int fd = channel->getSocket();
+	if (m_channelMap.find(fd) == m_channelMap.end()) {
+		return -1;
+	}
+	//todo...
+	int ret = m_dispatcher->modify();
+	return ret;
+}
+
+int EventLoop::remove(Channel* channel) {
+	int fd = channel->getSocket();
+	if(m_channelMap.find(fd) == m_channelMap.end()) {
+		return -1;
+	}
+	//todo...
+	int ret = m_dispatcher->remove();	
+	return ret;
+}
+
+void EventLoop::taskWakeup() {
+	const char* msg = "hello world!!!";
+	write(m_socketPair[0], msg, strlen(msg));
+}
+
+int EventLoop::readMessage() {
+	char buf[256];
+	read(m_socketPair[1], buf, sizeof(buf));
+	return 0;
+}
+
+int EventLoop::freeChannel(Channel* channel) {
+	auto it = m_channelMap.find(channel->getSocket());
+	if(it != m_channelMap.end()) {
+		m_channelMap.erase(it);
+		close(channel->getSocket());
+		delete channel;
+	}	
+	return 0;
+}
+
+
--- ReactorHttp/EventLoop.h
+++ ReactorHttp/EventLoop.h
@@ -4,6 +4,7 @@
 #include "Dispatcher.h"
 #include <mutex>
 #include <queue>
+#include "Channel.h"
 
 //处理节点的方式
 enum class ElemType : char { ADD, DELETE, MODIFY };
@@ -26,6 +27,26 @@ public:
 	int eventActive(int fd, int event);
 	//添加任务到任务队列中
 	int addTask(Channel* channel, ElemType type);
+	//处理任务队列中的任务
+	int processTaskQ();
+	//处理dispatcher中的节点
+	int add(Channel* channel);
+	int modify(Channel* channel);
+	int remove(Channel* channel);
+	//唤醒子线程后,子线程反应堆执行的本地套接字通信
+	int readMessage();
+	//释放channel
+	int freeChannel(Channel* channel);	
+	inline std::thread::id getThreadID() {
+		return m_threadID;
+	}
+
+	////静态成员函数是不依赖对象,配合this回传使用
+	//static int readLocalMessage(void* arg);
+
+private:
+	//唤醒子线程
+	void taskWakeup();
 
 private:
 	bool m_isQuit;//反应堆开关
