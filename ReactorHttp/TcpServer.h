ce453771e79d805539a12e142515877c16b1c23c
wz <1437175102@qq.com>
2026-02-02 22:04:27 +0800

    添加TcpServer模块启动服务器

--- ReactorHttp/TcpServer.h
+++ ReactorHttp/TcpServer.h
@@ -1,1 +1,20 @@
 #pragma once
+#include "EventLoop.h"
+#include "ThreadPool.h"
+
+class TcpServer {
+public:
+	TcpServer(unsigned short port, int threadNum);
+	//初始化监听
+	void setListen();
+	//启动服务器
+	void run();
+	static int acceptConnection(void* arg);
+
+private:
+	int m_threadNum;
+	EventLoop* m_mainLoop;//主反应堆
+	ThreadPool* m_threadPool;//线程池	
+	int m_lfd;//监听套接字
+	unsigned short m_port;//断开
+};
